b6418b73f57fdc9e5a2b83dbc95e19385571854a
Felicia <feliciadsl@yahoo.com>
2018-05-22 22:49:32 -0400

    First commit

---
+++ compareHeader.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" compareHeader.py """
+if __name__ == "__main__":
+    """ __main__ """
+    import requests
+    import sys
+
+    # Accept 2 URLS from command line
+    if len(sys.argv) == 3:
+        try:
+            rG = requests.get(sys.argv[1])
+            print("URL1: {}".format(sys.argv[1]))
+            # Print Header options of URL1
+            for kG, vG in rG.headers.items():
+                print("{}: {}".format(kG, vG))
+        except:
+            sys.exit(0)
+
+        try:
+            rC = requests.get(sys.argv[2])
+            print("\nURL2: {}".format(sys.argv[2]))
+            # Print Header options of URL2
+            for kC, vC in rC.headers.items():
+                print("{}: {}".format(kC, vC))
+        except:
+            sys.exit(0)
+    else:
+        print("Please enter 2 URLs")
